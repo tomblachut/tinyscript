c43141e60119f0407adf6ba6866cffe4b3ad4bd4
Tomasz Blachut <blachut.tomasz@gmail.com>
2023-06-28 22:21:00 +0200

    getLiteralType

--- src/check.ts
+++ src/check.ts
@@ -1,5 +1,6 @@
+import * as types from "@babel/types";
 import { Node } from "@babel/types";
-import { Type } from "./types";
+import { BooleanType, NumberType, StringType, Type } from "./types";
 import { todoType, withCache } from "./utils";
 
 export interface CheckContext {
@@ -15,7 +16,21 @@ export function check(node: Node, context: CheckContext) {
   }
 
   function getTypeImpl(node: Node): Type {
-    return todoType();
+    return getLiteralType(node) ?? todoType();
+  }
+
+  function getLiteralType(node: Node): Type | undefined {
+    if (types.isLiteral(node)) {
+      switch (node.type) {
+        case "BooleanLiteral":
+          return new BooleanType();
+        case "NumericLiteral":
+          return new NumberType();
+        case "StringLiteral":
+          return new StringType();
+      }
+    }
+    return undefined;
   }
 
   getType(node);
